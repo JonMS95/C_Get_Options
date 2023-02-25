0fedee470a27e93d6be6fe4ede4f58fa63e2087e
JonMS95 <jon_portugalete@hotmail.com>
2023-02-25 17:14:21 +0100

    Added private function prototypes to GetOptions.h

--- Source_files/GetOptions.h
+++ Source_files/GetOptions.h
@@ -14,4 +14,7 @@
 #define ERR_OPT_VALUE_OUT_OF_BOUNDS -3
 #define ERR_OPT_DOES_NOT_EXIST      -4
 
+void PreParseOptions(option_description* option_descr, int option_descr_size, char* opt_short);
+int ParseArguments(int argc, char** argv, option_description* option_descr, int option_descr_size, char* opt_short);
+
 #endif
